b513eb8a483fe3203dfafd8cc8047f621749e690
GlennLee112 <glennlee112@gmail.com>
2025-08-22 14:05:00 +0800

    2025.08.22
    - Reformatted Tkinter_Main structure (retain functions)
    - SQL_Practice update (over partion by practice)

---
+++ .ipynb_checkpoints/olist_query-checkpoint.py
@@ -0,0 +1,246 @@
+import sqlite3
+import os
+# from functools import reduce
+
+import pandas as pd
+# from openpyxl.styles.builtins import output
+
+# import Directory.File_Manager as p_fm
+import Directory.Personal_Pandas as p_pd
+# from datetime import datetime
+
+base:str = os.path.dirname(__file__)
+out_path:str = os.path.join(base,"Out")
+sqlite_path:str = os.path.join(base, "Dataset", "olist.sqlite")
+
+connection = sqlite3.connect(sqlite_path)
+
+# data from: https://www.kaggle.com/datasets/terencicp/e-commerce-dataset-by-olist-as-an-sqlite-database
+
+# 0. print tables inside sqlite database
+
+
+min_max_order_date = """SELECT os.order_purchase_timestamp
+                        FROM orders os;"""
+
+def query_out(result_in:pd.DataFrame,
+              base_path:str,
+              file_name:str,
+              extension_type:int=0,         # Output type is csv (0) by default
+              output_index:bool=False):     # Index disabled by default
+
+    # Dict holding extension possible
+    extension_dict = {0:".csv",
+                      1:".xlsx"}
+
+    # Select extension using extension_type
+    extension_used = extension_dict[extension_type]
+
+    # output_path fabrication / joining
+    output_path = os.path.join(base_path, file_name + extension_used)
+
+    # output type by extension type
+    if extension_type == 0:
+        result_in.to_csv(output_path, index = output_index)
+
+    elif extension_type == 1:
+        result_in.to_excel(output_path, index = output_index)
+
+
+# 1. Query function
+def max_min_sales_date(conn_use:str | sqlite3.Connection)->pd.DataFrame:
+    max_min_sales_date_query =  """SELECT 
+                                        MAX(os.order_purchase_timestamp) as max_min_date
+                                    FROM orders os
+                                    UNION ALL
+                                    SELECT 
+                                        MIN(os.order_purchase_timestamp)
+                                    FROM orders os
+                                    ;"""
+
+    if conn_use is str:
+        conn_use = sqlite3.connect(conn_use)
+
+    elif conn_use is sqlite3.Connection:
+        conn_use = conn_use
+
+
+    with conn_use:
+        result = pd.read_sql_query(max_min_sales_date_query, con=conn_use)
+
+    result['max_min_date'] = pd.to_datetime(result['max_min_date'])
+    result = result.sort_values(by=['max_min_date'], ignore_index=True)
+    # print(result.dtypes)
+    # print(result)
+    result['max_min_date'] = result['max_min_date'].dt.date
+
+    return result
+
+def seller_query_func(conn_use:sqlite3.Connection)-> None | pd.DataFrame:
+
+    seller_query = """SELECT 
+                        s.seller_id,
+                        s.seller_city,
+                        s.seller_state,
+                        s.seller_zip_code_prefix,
+                        g.geolocation_city
+                    FROM 
+                        sellers s
+                    JOIN 
+                        geolocation g ON s.seller_zip_code_prefix = g.geolocation_zip_code_prefix
+                    GROUP BY
+                        s.seller_id"""
+
+    with conn_use:
+        result = pd.read_sql_query(seller_query, con=conn_use)
+
+    return result
+
+def order_query_func(conn_use:sqlite3.Connection,
+                     min_date:str,
+                     max_date: str,
+                     output_path:str,
+                     data_only:bool=True) -> None | pd.DataFrame:
+
+    """New order query function:
+
+    Query orders and join with review score and items information to get the overall order information"""
+
+    # Query to use
+    orders_query = """SELECT
+                        o.order_id,
+                        o.customer_id,
+                        o.order_status,
+                        o.order_purchase_timestamp,
+                        o.order_approved_at,
+                        o.order_delivered_carrier_date,
+                        o.order_delivered_customer_date,
+                        o.order_estimated_delivery_date
+                    FROM orders o
+                    WHERE 
+                         o.order_purchase_timestamp BETWEEN ? AND ?;"""
+
+    order_seller_query = """SELECT
+                            oi.order_id,
+                            oi.seller_id,
+                            oi.price
+                        FROM 
+                            order_items oi
+                        JOIN orders o ON oi.order_id = o.order_id
+                        WHERE
+                            o.order_purchase_timestamp BETWEEN ? AND ?
+                        GROUP BY
+                            oi.order_id;"""
+
+    order_reviews_query = """SELECT
+                            ors.order_id,
+                            ors.review_score
+                        FROM 
+                            order_reviews ors
+                        JOIN orders o ON ors.order_id = o.order_id
+                        WHERE
+                            o.order_purchase_timestamp BETWEEN ? AND ?;"""
+
+    order_products_query = """SELECT
+                            o.order_id,
+                            ps.product_id,
+                            ps.product_category_name,
+                            ps.product_weight_g
+                        FROM 
+                            orders o
+                        JOIN order_items oi ON oi.order_id = o.order_id
+                        JOIN products ps ON oi.product_id = ps.product_id
+                        WHERE
+                            o.order_purchase_timestamp BETWEEN ? AND ?;"""
+
+    order_payment_query = """SELECT
+                            op.order_id,
+                            op.payment_sequential,
+                            op.payment_type,
+                            op.payment_installments,
+                            op.payment_value
+                        FROM 
+                            order_payments op
+                        JOIN orders o ON op.order_id = o.order_id
+                        WHERE
+                            o.order_purchase_timestamp BETWEEN ? AND ?;"""
+
+    product_cat_translation_query = """SELECT
+                                        pcnt.product_category_name,
+                                        pcnt.product_category_name_english
+                                    FROM
+                                        product_category_name_translation pcnt
+                                    JOIN products ps ON pcnt.product_category_name = ps.product_category_name
+                                    JOIN order_items oi ON ps.product_id = oi.product_id
+                                    JOIN orders o ON oi.order_id = o.order_id
+                                    WHERE
+                                        o.order_purchase_timestamp BETWEEN ? AND ?;"""
+
+
+
+    # Test query only; disable when in actual use
+    # order_items_query = """SELECT
+    #                         oi.order_id,
+    #                         oi.order_item_id
+    #                     FROM
+    #                         order_items oi
+    #                     JOIN products ps ON oi.product_id = ps.product_id
+    #                     JOIN orders o ON oi.order_id = o.order_id
+    #                     WHERE
+    #                         o.order_purchase_timestamp BETWEEN ? AND ?;"""
+    #
+    # review_comment_query = """SELECT
+    #                             ors.order_id,
+    #                             ors.review_comment_title,
+    #                             ors.review_comment_message
+    #                         FROM
+    #                             order_reviews ors
+    #                         JOIN orders o ON o.order_id = ors.order_id
+    #                         WHERE
+    #                             o.order_purchase_timestamp BETWEEN ? AND ?;"""
+
+
+
+    # Connect & query
+    with conn_use:
+        order_result = pd.read_sql_query(orders_query, con=conn_use, params=[min_date, max_date])
+        order_seller_result = pd.read_sql_query(order_seller_query, con=conn_use, params=[min_date, max_date])
+        order_reviews_result = pd.read_sql_query(order_reviews_query, con=conn_use, params=[min_date, max_date])
+        order_products_result = pd.read_sql_query(order_products_query, con=conn_use, params=[min_date, max_date])
+        order_payment_result = pd.read_sql_query(order_payment_query, con=conn_use, params=[min_date, max_date])
+        # order_items_result = pd.read_sql_query(order_items_query, con=conn_use, params=[min_date, max_date]) # testing only; disable or enable as required
+        # review_comment_result = pd.read_sql_query(review_comment_query, con=conn_use, params=[min_date, max_date]) # testing only; disable or enable as required
+        product_translation_result = pd.read_sql_query(product_cat_translation_query, con=conn_use,
+                                                       params=[min_date, max_date])
+
+
+    # joining
+    # list of df to join
+    df_list_to_use = [order_result, order_seller_result, order_reviews_result, order_products_result,
+                      order_payment_result]
+
+    # join 1 -- join review and
+    final_df = p_pd.df_batch_merge(df_list_to_use, "order_id", "3")
+    # test query order items
+
+    # product translation query and drop duplicate
+    product_translation_result_final = product_translation_result.drop_duplicates()
+
+    # Query_out if not data_only (i.e.: data_only=False)
+    if not data_only:
+        query_out(final_df, output_path, "order overall")
+        query_out(product_translation_result_final, output_path, "product cat translation")
+        # query_out(order_items_result, output_path, "order_items") # testing only; disable or enable as required
+        # query_out(review_comment_result, output_path, "review comment") # testing only; disable or enable as required
+
+    # Return final_df
+    return final_df
+
+# def product_category_en(conn_use:sqlite3.Connection,
+#                      min_date:str,
+#                      max_date: str,
+#                      output_path:str,
+#                      data_only:bool=True) -> None | pd.DataFrame:
+#
+
+# data output for use by Tableau
--- Py_Practice-Tkinter_Main.py
+++ Py_Practice-Tkinter_Main.py
@@ -20,7 +20,13 @@ from olist_query import (query_out,
 from threading import Thread
 
 # Reference :
+# The following are reference used in
 # https://stackoverflow.com/questions/71345245/frame-inside-a-oop-tkinter-frame
+
+# Explanation of inheritance and Object-Oriented Programming (included ChatGPT explanation):
+# https://www.geeksforgeeks.org/python/python-super-with-__init__-method/
+# https://chatgpt.com/s/t_68a536130ed48191a98a91986ae606e7
+# https://chatgpt.com/s/t_68a5358d45648191bebc057a0279ca5c
 
 # path manage
 base:str = os.path.dirname(__file__)
@@ -56,116 +62,116 @@ class Application(Tk):
         self.title_frame = Frame(self)
         self.title_frame.grid(row=0, column=0, columnspan=2, pady=10, padx=10)
         # Instantiate and pack the TitleFrame
-        self.title_frame_widget = self.Query_frame(self.title_frame)
+        self.title_frame_widget = Query_frame(self.title_frame)
         self.title_frame_widget.grid(row=0, column=0)
 
-    # 1.1 Title frame
-    class Query_frame(Frame):
-        def __init__(self,parent):
-            super().__init__(parent)
-            self.title_frame = Frame(self)
-            self.title_frame.grid(row=0, column=0, columnspan=2, pady=10, padx=10)
-            # Instantiate and pack the TitleFrame
-            self.title_frame_widget = self.TitleFrame(self.title_frame)
-            self.title_frame_widget.grid(row=0, column=0)
+# 1.1 Title frame
+class Query_frame(Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.title_frame = Frame(self)
+        self.title_frame.grid(row=0, column=0, columnspan=2, pady=10, padx=10)
+        # Instantiate and pack the TitleFrame
+        self.title_frame_widget = TitleFrame(self.title_frame)
+        self.title_frame_widget.grid(row=0, column=0)
 
-            # 2.2 date_frame
-            self.date_frame = Frame(self)
-            self.date_frame.grid(row=1, column=0, pady=10, padx=10, rowspan=4)
-            # Instantiate and pack the DateSelectorFrame
-            self.date_selector_widget = self.DateSelectorFrame(self.date_frame)
-            self.date_selector_widget.grid(row=0, column=0)
+        # 2.2 date_frame
+        self.date_frame = Frame(self)
+        self.date_frame.grid(row=1, column=0, pady=10, padx=10, rowspan=4)
+        # Instantiate and pack the DateSelectorFrame
+        self.date_selector_widget = DateSelectorFrame(self.date_frame)
+        self.date_selector_widget.grid(row=0, column=0)
 
-        class TitleFrame(Frame):
-            def __init__(self, parent):
-                super().__init__(parent)
-                self.configure(height=40)
-                # Labels strings
-                title_label_text = "Select the date range to be queried:"  # Text
+class TitleFrame(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(height=40)
+        # Labels strings
+        title_label_text = "Select the date range to be queried:"  # Text
 
-                # title
-                title = tk.Label(self, text=title_label_text)
-                title.grid()
+        # title
+        title = tk.Label(self, text=title_label_text)
+        title.grid()
 
             # 1.2 Date select frame
-        class DateSelectorFrame(Frame):
-            def __init__(self, parent):
-                super().__init__(parent)
+class DateSelectorFrame(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-                # querying data using
-                def query_run():
-                    # get date from DateSelect()
-                    end_date = self.end_date_selector.get_date()
-                    start_date = self.start_date_selector.get_date()
-                    print(end_date)
-                    print(start_date)
+        # querying data using
+        def query_run():
+            # get date from DateSelect()
+            end_date = self.end_date_selector.get_date()
+            start_date = self.start_date_selector.get_date()
+            print(end_date)
+            print(start_date)
 
-                    # top level for query dialog
-                    query_ongoing_dialog = tk.Toplevel()
-                    query_ongoing_dialog.geometry("350x250")
+            # top level for query dialog
+            query_ongoing_dialog = tk.Toplevel()
+            query_ongoing_dialog.geometry("350x250")
 
-                    # main function run
-                    def main():
-                        try:
-                            # query dialog labeling & packing
-                            query_ongoing_dialog_label = Label(query_ongoing_dialog, text="Querying")
-                            query_ongoing_dialog_label.pack()
+            # main function run
+            def main():
+                try:
+                    # query dialog labeling & packing
+                    query_ongoing_dialog_label = Label(query_ongoing_dialog, text="Querying")
+                    query_ongoing_dialog_label.pack()
 
-                            # Conn 1 querying
-                            # with connection as conn_1:
-                            #     df_1 = seller_query_func(conn_1)
-                                # print(df_1)
-                            # Conn 2 querying
-                            with connection as conn_2:
-                                df_2 = order_query_func(conn_2, start_date, end_date, output_path=out_path,
-                                                               data_only=False)
-                                # print(df_2)
+                    # Conn 1 querying
+                    # with connection as conn_1:
+                    #     df_1 = seller_query_func(conn_1)
+                        # print(df_1)
+                    # Conn 2 querying
+                    with connection as conn_2:
+                        df_2 = order_query_func(conn_2, start_date, end_date, output_path=out_path,
+                                                       data_only=False)
+                        # print(df_2)
 
-                            query_ongoing_dialog.after(0, lambda: query_ongoing_dialog_label.
-                                                       config(text='Completed'))
+                    query_ongoing_dialog.after(0, lambda: query_ongoing_dialog_label.
+                                               config(text='Completed'))
 
-                            query_ongoing_dialog.after(2500, query_ongoing_dialog.destroy)
+                    query_ongoing_dialog.after(2500, query_ongoing_dialog.destroy)
 
-                        except Exception as e:
-                            query_ongoing_dialog.after(0, query_ongoing_dialog.destroy)
-                            messagebox.showerror('Error', f"An error has occurred:{e}")
+                except Exception as e:
+                    query_ongoing_dialog.after(0, query_ongoing_dialog.destroy)
+                    messagebox.showerror('Error', f"An error has occurred:{e}")
 
-                    # Thread start
-                    Thread(target=main).start()
+            # Thread start
+            Thread(target=main).start()
 
-                # 1. Text
-                self.configure(height=10, width=10)
-                # 1.1 Date Start
-                start_date_text = ("Start Date \n"
-                                   "(yyyy/mm/dd):")
-                start_date_label = Label(self, text = start_date_text)
-                # 1.2 Date End
-                end_date_text = ("End Date \n"
-                                 "(yyyy/mm/dd):")
-                end_date_label = tk.Label(self, text = end_date_text)
-                # 1.3 Text packing
-                # start
-                start_date_label.grid(row=0, column=0)
-                # end
-                end_date_label.grid(row=2, column=0)
-                # # 2. Selector
-
-
-                # Start
-                self.start_date_selector = tkcalendar.DateEntry(self, mindate=min_date, maxdate=max_date,
-                                                                date_pattern="yyyy/mm/dd")
-                self.start_date_selector.grid(row=0, column=1)
-                self.start_date_selector.set_date(min_date) # set start date to min date
-
-                # End
-                self.end_date_selector = tkcalendar.DateEntry(self, mindate=min_date, maxdate=max_date,
-                                                              date_pattern  = "yyyy/mm/dd")
-                self.end_date_selector.grid(row=2, column=1)
-                self.end_date_selector.set_date(max_date) # set end date to max date
+        # 1. Text
+        self.configure(height=10, width=10)
+        # 1.1 Date Start
+        start_date_text = ("Start Date \n"
+                           "(yyyy/mm/dd):")
+        start_date_label = Label(self, text = start_date_text)
+        # 1.2 Date End
+        end_date_text = ("End Date \n"
+                         "(yyyy/mm/dd):")
+        end_date_label = tk.Label(self, text = end_date_text)
+        # 1.3 Text packing
+        # start
+        start_date_label.grid(row=0, column=0)
+        # end
+        end_date_label.grid(row=2, column=0)
+        # # 2. Selector
 
 
-                self.button_run = tk.Button(self, text="Run", command=query_run)
-                self.button_run.grid(row=3,column=0, columnspan=2)
+        # Start
+        self.start_date_selector = tkcalendar.DateEntry(self, mindate=min_date, maxdate=max_date,
+                                                        date_pattern="yyyy/mm/dd")
+        self.start_date_selector.grid(row=0, column=1)
+        self.start_date_selector.set_date(min_date) # set start date to min date
+
+        # End
+        self.end_date_selector = tkcalendar.DateEntry(self, mindate=min_date, maxdate=max_date,
+                                                      date_pattern  = "yyyy/mm/dd")
+        self.end_date_selector.grid(row=2, column=1)
+        self.end_date_selector.set_date(max_date) # set end date to max date
+
+
+        self.button_run = tk.Button(self, text="Run", command=query_run)
+        self.button_run.grid(row=3,column=0, columnspan=2)
 
 
 if __name__ == '__main__':
